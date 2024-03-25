0361e075edfb668047b2075308c9049a2ded2cf3
edwinsupremacy <edwinmwangi837@gmail.com>
2024-03-25 14:56:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply =num1*num2
+
+let random = Math.ceil(Math.random()*100);
+
+let num3=32
+let num4=8
+let mod= num3/num4
+
+let max= Math.max(20,8,1,13)
